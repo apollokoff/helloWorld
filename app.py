263892ee230620e9fd5e392bdcdae228ca48c4ea
apollokoff <apollo15@terpmail.umd.edu>
2024-03-26 19:21:07 -0400

    Created templates that incorporate requests and parent child relationships, and  also updated app.py to create routes for these pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,21 @@ def aboutcss():  # put application's code here
 @app.route('/hello')
 def hello():  # put application's code here
     return render_template('hello.html')
+
+@app.route('/favorite-course')
+def favorite_course():  # put application's code here
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():  # put application's code here
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
